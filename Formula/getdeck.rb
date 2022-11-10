241e1f2066bea0f0838738f7bf1e91eb0586688f
runner <runner@Mac-1668067558347.local>
2022-11-10 08:20:58 +0000

    getdeck 0.11.1

--- Formula/getdeck.rb
+++ Formula/getdeck.rb
@@ -1,8 +1,8 @@
 class Getdeck < Formula
   desc "Reproducible Kubernetes environments for development and testing"
   homepage "https://getdeck.dev/"
-  url "https://github.com/Getdeck/getdeck/releases/download/0.11.0/deck-0.11.0-darwin-universal.zip"
-  sha256 "0bae3f05a98f91a7cf561d31d8bec45f7fab724f9f51b7b5ee9bf4fe666d330b"
+  url "https://github.com/Getdeck/getdeck/releases/download/0.11.1/deck-0.11.1-darwin-universal.zip"
+  sha256 "21dc26cb6bd6700288b885483e4a2b14fd536ff929ba18d51b52644da3268294"
   license "Apache-2.0"
 
   def install
